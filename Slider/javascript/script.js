cb2bbbf9434275951e517227dff900d23cd4df87
MasudRana951 <118680879+MasudRana951@users.noreply.github.com>
2022-11-20 23:37:19 +0600

    created file
    created all the files and codes just now. and uploaded it here

---
+++ Slider/javascript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider_start').slick({
+    autoplay: true,
+    arrows: false,
+    dots: true,
+    // prevArrow: <button type="button" class="slick-prev">Previous</button>,
+    });
+  });
